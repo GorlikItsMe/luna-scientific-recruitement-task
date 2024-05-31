9cbe5ae18514345abbd71ab53d074b1353ff0547
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2024-05-31 17:14:42 +0200

    Add swagger docs

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Hydroponic system API",
+        default_version='v1',
+        description="luna scientific recruitement task",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('app.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.TOOLBAR_ENABLE and settings.DEBUG:
